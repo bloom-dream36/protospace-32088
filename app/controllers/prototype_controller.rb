3a7e4292fc36a6236a5ff4c7f61dff98375bd5fc
bloom-dream36 <dream.2001.samu@icloud.com>
2020-11-28 19:04:10 +0900

    syantaxerrore　修正

--- app/controllers/prototype_controller.rb
+++ app/controllers/prototype_controller.rb
@@ -1,7 +1,7 @@
 class PrototypeController < ApplicationController
  before_action :set_prototype, only: [:edit, :show]
  before_action :authenticate_user!, only: [:edit, :update, :destroy]
- before_action :prototype_user, expect: [:index, :show, :create :new]
+ before_action :prototype_user, expect: [:index, :show, :create ,:new]
   def index
     @prototypes = Prototype.includes(:user)
     
