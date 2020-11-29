da5ecc3ad97beba7808941873a7a759c44ec47fb
bloom-dream36 <dream.2001.samu@icloud.com>
2020-11-29 20:08:28 +0900

    コメントが空だとshowに戻る

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,15 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = Comment.new(comment_params)
+      @comment = Comment.new(comment_params)
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
       @prototype = @comment.prototype
       @comments = @prototype.comments
-      render "prototype/show"
+      render "prototypes/show"
     end
-  end
+  end 
   
   private
    def comment_params
